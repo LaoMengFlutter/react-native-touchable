5ae46a572e40ee21af8b27c3caa06868ee2325ca
孟庆东 <781238222@qq.com>
2017-07-18 12:05:02 +0800

    Add

---
+++ example/TouchableDemo.js
@@ -0,0 +1,95 @@
+/**
+ * Created by mengqingdong on 2017/4/19.
+ */
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+} from 'react-native';
+import Touchable from '../lib/Touchable'
+
+export default class TouchableDemo extends Component {
+
+    render() {
+        return (
+            <View style={{flex:1,marginTop:30}}>
+                <Touchable
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={{marginTop:20,backgroundColor:'#cccccc'}}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={{marginTop:20,borderColor:'#cccccc',borderWidth:1,backgroundColor:'#cccccc00'}}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={{marginTop:20,height:50,width:150,backgroundColor:'#cccccc'}}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={{marginTop:20,height:50,width:150,backgroundColor:'#cccccc',justifyContent:'flex-start',alignItems:'flex-start'}}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={styles.touchable}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+                <Touchable
+                    style={[styles.touchable,{backgroundColor:'blue',}]}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+
+
+                <Touchable
+                    activeOpacity={0.3}//此属性只对ios有效
+                    style={[styles.touchable,{backgroundColor:'blue',}]}
+                    onPress={()=>{
+                       console.warn('点我');
+                    }}>
+                    <Text>点我</Text>
+                </Touchable>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    touchable:{
+        marginTop:20,
+        height:50,
+        width:150,
+        backgroundColor:'#cccccc',
+        alignItems:'flex-start',
+        paddingLeft:20
+    }
+})
---
+++ index.android.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {
+  AppRegistry,
+
+} from 'react-native';
+
+import Touchable from './example/TouchableDemo'
+
+AppRegistry.registerComponent('Touchable', () => Touchable);
